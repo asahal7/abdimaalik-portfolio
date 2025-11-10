b1f811486727b36b55cb3242e635bf05c025908d
Abdimaalik Sahal <abdimaaaalik0@gmail.com>
2025-11-10 21:43:07 +0000

    href bug fixed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -53,8 +53,8 @@ const Contact = () => {
 
       {/* Resume Download Button */}
       <a
-        href="./AbdimaalikSahalCV.pdf"
-        download="AbdimaalikSahalCV.pdf"
+        href="/AbdimaalikSahalCV.pdf" 
+        download
         className="inline-block mt-6 px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition"
       >
         Download Resume
